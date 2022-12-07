77f8fdb8dcb058d67dceb2e4f1c5c2688ffabe12
Kemily <kemily.r.heleno@gmail.com>
2022-12-07 16:56:10 -0300

    Código na prática - JAVA

---
+++ Bootcamp/Curso - Seja protagonista nesse bootcamp/Multiplicacao_simples.java
@@ -0,0 +1,27 @@
+                /* DESAFIO */
+/*  Você receberá dois valores inteiros. Faça a eitura e em seguida calcule o produto entre estes dois valores.
+    Atribuia esta operação à variavel PROD, mostrando esta de acordo com a mensagem de saída esperada
+
+    ENTRADA: A entrada contém 2 valores inteiros.
+    Saída: Exiba a variável PROD conforme exeplo abaixo, tendo obrigatoriedade um espaço em branco anes e depois da igualdade
+*/
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        
+    Scanner leitor = new Scanner(System.in);
+
+    int A, B, PROD;
+
+    A = leitor.nextInt();
+    B = leitor.nextInt();
+
+    PROD = ; // Implemente o código que reresenta a multiplicação
+
+    System.out.println("PROD = " + );
+
+    leitor.close();
+    }
+}
